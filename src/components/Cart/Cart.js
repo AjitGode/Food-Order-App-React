17e548e8a7a94bfa8f772d1fadd50ae7d5439287
ajit2599 <godeajit71@gmail.com>
2023-06-28 09:52:18 +0530

    React-Project

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,22 @@
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css'
+const Cart = (props) => {
+    const cartItem = <ul className={classes['cart-items']}>
+        {[{ id: '1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => <li>{item.name}</li>)}
+    </ul>;
+    return (
+        <Modal onClose={props.onClose}>
+            {cartItem}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.63</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </Modal>
+    )
+}
+
+export default Cart;
